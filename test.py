fc531fc3b488da34f2552be9de2767bac3ce6c72
aitymori <anakholodko@gmail.com>
2023-01-14 03:20:39 +0200

    Create classes and functions
    All Ok

---
+++ test.py
@@ -0,0 +1,56 @@
+from collections import UserDict
+
+
+class Field():
+    pass
+
+
+class Name(Field):
+
+    def __init__(self, name):
+        self.name = name
+
+
+class Phone(Field):
+    
+    def __init__(self, phone):
+        self.value = phone
+
+
+class Record():
+
+    def __init__(self, name: Name, *phones):
+        self.name = name
+        self.phones = list(phones)
+
+    def add_phone(self, value: Phone):
+        self.phones.append(value)
+    
+    def change_phone(self, phone_old: Phone, phone_new: Phone):
+        self.phones.remove(phone_old)
+        self.phones.append(phone_new)
+
+    def del_phone(self, phone: Phone):
+        self.phones.remove(phone)
+
+
+class AddressBook(UserDict):
+
+    def add_record(self, record: Record):
+        self.data[record.name.name] = record
+
+
+if __name__ == '__main__':
+    name = Name('Bill')
+    phone = Phone('1234567890')
+    rec = Record(name, phone)
+    ab = AddressBook()
+    ab.add_record(rec)
+    
+    assert isinstance(ab['Bill'], Record)
+    assert isinstance(ab['Bill'].name, Name)
+    assert isinstance(ab['Bill'].phones, list)
+    assert isinstance(ab['Bill'].phones[0], Phone)
+    assert ab['Bill'].phones[0].value == '1234567890'
+    
+    print('All Ok)')
